2d4b2f6deb5e20b300361f38d38c3c9062899dd6
JoseLuis1411 <perezlarajose14@gmail.com>
2025-08-11 11:34:46 -0600

    Reusmen de la api

--- app.js
+++ app.js
@@ -31,43 +31,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json()); //Para poder recibir datos en formato JSON
 
-const resumen = `###Recupera todas las peliculas
-GET http://localhost:3000/peliculas
-
-
-###Recupera una pelicula por id
-GET http://localhost:3000/peliculas/dcdd0fad-a94c-4810-8acc-5f108d3b18c3
-
-
-###Recupera una pelicula por genero
-GET http://localhost:3000/peliculas?genre=Action
-
-
-###Crea una pelicula
-POST http://localhost:3000/peliculas
-Content-Type: application/json
-
-{
-  "title": "Inception",
-  "year": 2010,
-  "director": "Christopher Nolan",
-  "rate": 5,
-  "poster": "https://example.com/inception.jpg",
-  "duration": 148,
-  "genre": ["Science Fiction"]
-} 
-
-
-###Actualizar una pelicula
-PATCH http://localhost:3000/peliculas/dcdd0fad-a94c-4810-8acc-5f108d3b18c3
-Content-Type: application/json
-
-{
-  "year": 1889
-}`;
-
 app.get("/", (req, res) => {
-  res.send("Resumen API REST \n" + resumen);
+  res.send("Resumen API REST");
 });
 
 //Hya un middleware par aoslucionar lso probelmas de corrs pero lo que hace es permitir todo y no es seguro ni util simepre
